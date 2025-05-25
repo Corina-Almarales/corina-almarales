6b488d913499630ef2efba10d5dbf313916e3bdf
Corina-Almarales <corinalmarales@gmail.com>
2025-05-25 17:42:20 -0600

    Updated Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+import './Navbar.scss';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Constante Florecer</div>
+      <ul className="nav-links">
+        <li><a href="#inicio">Inicio</a></li>
+        <li><a href="#sobre-mi">Sobre mí</a></li>
+        <li><a href="#servicios">Servicios</a></li>
+        <li><a href="#tarifas-contacto">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
