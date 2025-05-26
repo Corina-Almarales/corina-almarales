5022756c1ff1b7842513b4878b6395038f1d335b
Corina-Almarales <corinalmarales@gmail.com>
2025-05-25 22:20:22 -0600

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
-'use client';
-
 import './global.css';
 import type { Metadata } from 'next';
 import Navbar from './components/Navbar';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import AOSProvider from './components/AOSProvider';
 
 export const metadata: Metadata = {
   title: 'Corina Almarales | Creativa',
@@ -17,23 +13,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,  // Duración de la animación
-      once: true,      // Solo se anima una vez al hacer scroll
-    });
-  }, []);
-
   return (
     <html lang="es">
       <body>
-        <Navbar />
-        <main>{children}</main>
-        <footer className="footer">
-          <p className="text-sm text-center text-gray-500 mt-8">
-            © {new Date().getFullYear()} Corina Almarales. Todos los derechos reservados.
-          </p>
-        </footer>
+        <AOSProvider>
+          <Navbar />
+          <main>{children}</main>
+          <footer className="footer">
+            <p className="text-sm text-center text-gray-500 mt-8">
+              © {new Date().getFullYear()} Corina Almarales. Todos los derechos reservados.
+            </p>
+          </footer>
+        </AOSProvider>
       </body>
     </html>
   );
