e449d9e7685bdd472e379c101f2c62f99177f2f2
Corina-Almarales <corinalmarales@gmail.com>
2025-05-25 18:34:03 -0600

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,7 @@ import Navbar from './components/Navbar';
 export default function HomePage() {
   return (
     <>
-      <nav className="navbar">
-        <div className="logo">Corina Almarales</div>
-        <ul className="nav-links">
-          <li><a href="#inicio">Inicio</a></li>
-          <li><a href="#sobre-mi">Sobre mí</a></li>
-          <li><a href="#servicios">Servicios</a></li>
-          <li><a href="#tarifas-contacto">Contacto</a></li>
-        </ul>
-      </nav>
+  
 
       <section id="inicio" className="hero">
         <h1 className="nombre">Corina Almarales</h1>
