217b9869011f7dc88b6436a14bb68868731809aa
Corina-Almarales <corinalmarales@gmail.com>
2025-05-25 18:44:25 -0600

    Updated Navbar.tsx

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Corina Almarales</div>
+      <ul className="nav-links">
+        <li><a href="#inicio">Inicio</a></li>
+        <li><a href="#sobre-mi">Sobre mí</a></li>
+        <li><a href="#servicios">Servicios</a></li>
+        <li><a href="#tarifas-contacto">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
