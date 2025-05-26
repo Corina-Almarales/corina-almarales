8f2373dc08994c2a1a6cb01464c9e865cee1126c
Corina-Almarales <corinalmarales@gmail.com>
2025-05-25 20:02:51 -0600

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,6 @@ import type { Metadata } from 'next';
 import React from 'react';
 import Navbar from './components/Navbar';
 
-export default function HomePage() {
-  return (
-    <>
-      <Navbar />
-      
 export const metadata: Metadata = {
   title: 'Corina Almarales | Portfolio',
   description: 'Servicios Creativos y Académicos',
@@ -22,7 +17,9 @@ export default function RootLayout({
     <html lang="es">
       <body>
         <Navbar />
-        <main>{children}</main>
+        <main style={{ paddingTop: '100px' }}>
+          {children}
+        </main>
         <footer className="footer">
           <p className="text-sm text-center text-gray-500 mt-8">
             © {new Date().getFullYear()} Corina Almarales. Todos los derechos reservados.
